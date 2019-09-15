160f90e0d7c74c89a7a11bbf4e7b72cf60a43efa
ChexterWang@desk <tos515541905@gmail.com>
2019-09-16 00:53:02 +0800

    little change to see if the address change

--- pr/pr1/pr103.cpp
+++ pr/pr1/pr103.cpp
@@ -4,12 +4,13 @@ using namespace std;
 
 int main(int argc, char const *argv[]) {
 
-  int a;
+  int a, b;
   int *p;
   cout << "a@: " << &a << endl;
   cout << "*p@: " << &p << endl;
   p = &a;
   cout << "*p@: " << &p << endl;
+  cout << "b@: " << &b << endl;
 
   return 0;
 }
