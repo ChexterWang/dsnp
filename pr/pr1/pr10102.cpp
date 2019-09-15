75fbf50dbeb48e8038c9dda8794503e22140e25c
ChexterWang@desk <tos515541905@gmail.com>
2019-09-15 16:51:33 +0800

    add pr10102 03

---
+++ pr/pr1/pr10102.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int b, a, c;
+  cout << "&b = " << &b << endl;
+  cout << "&a = " << &a << endl;
+  cout << "&c = " << &c << endl;
+}
